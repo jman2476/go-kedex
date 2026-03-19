2d4d169131a44349f1f22dd741070245f4a18372
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-03-19 00:35:19 -0400

    add cleanInput function

--- repl.go
+++ repl.go
@@ -1,7 +1,29 @@
 package main
 
-func cleanInput(text string)  []string {
-	var words []string
+import (
+	"fmt"
+)
 
+func cleanInput(text string) []string {
+	var words []string
+	var word string
+
+	for _, c := range text {
+		if c == 32 {
+			if word != "" {
+				words = append(words, word)
+				word = ""
+			}
+			continue
+		}
+		fmt.Print(text)
+		word = word + fmt.Sprintf("%c", c)
+		fmt.Println(word)
+	}
+	if word != "" {
+		words = append(words, word)
+	}
+
+	fmt.Printf("Resulting slice of strings: %v", words)
 	return words
 }
