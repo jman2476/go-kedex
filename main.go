a081723a2bb6db6a22082298a17b4e4eb8125e27
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-03-19 01:11:08 -0400

    add pokedex command prompt

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		fmt.Printf("Your command was: %s\n", cleaned[0])
+	}
 }
