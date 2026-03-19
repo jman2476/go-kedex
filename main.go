8a2afb38a6d762e2a084781f3fe960e933ae247b
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-03-19 16:19:54 -0400

    add command registry, and exit and help commands

--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	commandRegistry := map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-	}
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		text := scanner.Text()
-		cleaned := cleanInput(text)
-		command, ok := commandRegistry[cleaned[0]]
-		if ok {
-			command.callback()
-		} else {
-			fmt.Println("Command not recognized")
-		}
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,10 +1,60 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var registry = map[string]cliCommand{
+	// "exit": {
+	// 	name:        "exit",
+	// 	description: "Exit the Pokedex",
+	// 	callback:    commandExit,
+	// },
+	// "help": {
+	// 	name:        "help",
+	// 	description: "Displays a help message",
+	// 	callback:    commandHelp,
+	// },
+}
+
+func init() {
+	registry["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+	registry["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		command, ok := registry[cleaned[0]]
+		if ok {
+			command.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
@@ -17,8 +67,11 @@ func commandExit() error {
 	return nil
 }
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+
+	for _, command := range registry {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
 }
