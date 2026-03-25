95d66c0c5c1e93267c99da4371a0fd0b87e098a4
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-03-25 15:48:16 -0400

    change formatting inspect command

--- command-inspect.go
+++ command-inspect.go
@@ -28,7 +28,7 @@ func printStats(pokemon apicaller.PokemonInfo) {
 	fmt.Printf("Base experience: %d\n", pokemon.BaseExperience)
 	fmt.Printf("Stats:\n")
 	for idx, stat := range pokemon.Stats {
-		fmt.Printf("	-%s: %d", stat.Stat.Name, stat.BaseStat)
+		fmt.Printf(" -%s: %d", stat.Stat.Name, stat.BaseStat)
 		if idx%3 == 0 {
 			fmt.Print("\n")
 		}
@@ -36,7 +36,7 @@ func printStats(pokemon apicaller.PokemonInfo) {
 	fmt.Print("\n")
 	fmt.Printf("Abilities:\n")
 	for idx, ability := range pokemon.Abilities {
-		fmt.Printf("	-%s", ability.Ability.Name)
+		fmt.Printf(" -%s", ability.Ability.Name)
 		if idx%3 == 0 {
 			fmt.Print("\n")
 		}
@@ -44,7 +44,7 @@ func printStats(pokemon apicaller.PokemonInfo) {
 	fmt.Print("\n")
 	fmt.Printf("Moves:\n")
 	for idx, move := range pokemon.Moves {
-		fmt.Printf("	-%s", move.Move.Name)
+		fmt.Printf(" -%s", move.Move.Name)
 		if idx%3 == 0 {
 			fmt.Print("\n")
 		}
@@ -52,7 +52,7 @@ func printStats(pokemon apicaller.PokemonInfo) {
 	fmt.Print("\n")
 	fmt.Printf("Types:\n")
 	for idx, pokeType := range pokemon.Types {
-		fmt.Printf("	-%s", pokeType.Type.Name)
+		fmt.Printf(" -%s", pokeType.Type.Name)
 		if idx%3 == 0 {
 			fmt.Print("\n")
 		}
